923f053fa9d711efa61cf0c85a2a95df6938da92
Saipheddine <seifeddinebouhouch24@gmail.com>
2020-10-25 21:51:48 +0100

    filled leser and buecher tables

--- bibliothek.sql
+++ bibliothek.sql
@@ -8,15 +8,24 @@ create table leser
     nachname     varchar(30) not null
 );
 
+insert into leser (vorname, nachname) values ("a", "b"), ("c","d"), ("e","f"),("g","h");
+select * from leser;
+
+
 drop table if exists buecher;
 create table buecher
 (
     buch_id         int unsigned auto_increment primary key,
+    title           varchar(70) not null,
     autor           varchar(100) not null,
 	kategorie       enum('K', 'W', 'U'),
     ausleihanzahl   int unsigned default 0,
     ausleihbar     	bool default false
 );
+
+insert into buecher (autor, title, kategorie, ausleihanzahl, ausleihbar)
+values ("chekspear", 'Love', 'K', 2, true), ('Seif', 'SQL Basics','W', 0, false), ("Steven Hawking", "Black Holes", 'W', 4, true), ("Victor Hugo", "Les amis", "U", 1, true);
+select * from buecher;
 
 drop table if exists verleihen;
 create table verleihen
